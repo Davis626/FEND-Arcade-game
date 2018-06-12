e73c0baa1be59e7768de6373714234680dd68e1b
Davis <davis.sermons@gmail.com>
2018-06-12 23:29:27 +0300

    refactor: add code comments

--- js/app.js
+++ js/app.js
@@ -30,8 +30,6 @@ Enemy.prototype.update = function(dt) {
     this.x = -100;
     this.speed = 100 + Math.floor(Math.random() * 500);
   }
-
-
 };
 
 // Place enemies randomly
@@ -39,43 +37,38 @@ enemyPosition.forEach(function (positionY) {
     enemy = new Enemy(-100, positionY, 100 + Math.floor(Math.random() * 500));
     allEnemies.push(enemy);
 });
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 /*
  * Create player
  */
 
+// creating player class
  class Player {
+   // Object keys - position on X, Y axis, image
    constructor(x, y) {
      this.x = x;
      this.y = y;
      this.sprite = 'images/char-horn-girl.png';
    }
 
+   // Draw the player on the screen
    render() {
      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
    }
 
    update() {
+     //Return player to starting position and add points after reaching the water
      if (this.y < 0) {
        this.y = 1;
        setTimeout(() => this.reset(), 500);
        game.scorePoints();
      }
 
-     //if the player is on the same space as a enemy, bring him back home
+     //Check for collision with enemy bugs
      if (this.y === 68 || this.y === 151 || this.y ===  234) {
        let thisPlayer = this;
        allEnemies.forEach(function(enemy){
@@ -87,15 +80,16 @@ Enemy.prototype.render = function() {
      }
    }
 
+   //Reset to starting position
    reset() {
      this.x = 202;
      this.y = 400
    }
 
+   //Move player inside canvas
    handleInput(key) {
      switch (key) {
        case "left":
-       //if the left key is pressed, move left one column, but not off screen
        this.x - 100 >= 0 ? this.x = this.x - 100 : undefined;
        break;
 
@@ -119,12 +113,10 @@ Enemy.prototype.render = function() {
 let player = new Player(202, 400);
 
 
-
 /*
- * Keypress functionality
+ * Event listener for keypress - provided by Udacity
  */
 
-// Event listener for keypress - provided by Udacity
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
